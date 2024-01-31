4ec1d74df93fb8868f8b294e50d4b0f78f2b18c7
rabin-thami <rabinthami36@gmail.com>
2024-01-31 22:42:28 +0545

    first commit

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <header>
+            <nav>
+                <span>Rabin Thami</span>
+                <ul>
+                    <li><a href="">Project</a></li>
+                    <li><a href="">About Me</a></li>
+                    <li><a href="">Contact Me</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
