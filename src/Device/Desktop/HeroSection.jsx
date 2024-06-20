dcd0eb4f625fa88a7e00bc220875563b34e614a0
rabinthami36@gmail.com <rabinthami36@gmail.com>
2024-06-20 11:15:12 +0545

    New version

--- src/Device/Desktop/HeroSection.jsx
+++ src/Device/Desktop/HeroSection.jsx
@@ -1,48 +1,38 @@
-import HeroSectionImage from "/profile.png";
-
+import profileImage from "/image/profile.webp";
 export default function HeroSection() {
   return (
-    <div className="background-grid">
-      <div className="rabin relative">
-        <div className="flex items-center justify-center relative w-full h-2/3 -z-10">
-          <div className="relative w-1/4">
-            <img
-              src={HeroSectionImage}
-              alt="Profile Image"
-              className="rounded-t-full border-4 w-full"
-            />
-
-            <div
-              className="absolute inset-0"
-              style={{
-                background:
-                  "linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) 50%)",
-              }}
-            ></div>
-          </div>
+    <section className="magicpattern container mx-auto flex h-screen w-full max-w-6xl flex-col items-center justify-center">
+      {/* Image Section */}
+      <div className="relative flex w-1/4 overflow-hidden">
+        <img
+          src={profileImage}
+          alt="Hero Section image"
+          className="h-full w-full rounded-t-full border-4"
+        />
+        <div
+          className="absolute inset-0"
+          style={{
+            background:
+              "linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) 40%)",
+          }}
+        ></div>
+      </div>
+      {/* Text Section */}
+      <div className="z-10 -my-10">
+        <div className="space-y-3 text-center text-6xl font-medium">
+          <h1 className="">Hey, I'm Rabin Thami.</h1>
+          <h1 className="">Front-End Developer</h1>
         </div>
-        <div className="absolute left-1 right-1 top-1/2">
-          <h1 className="text-5xl z-10 font-medium leading-normal">
-            Hey, I'm <span>Rabin Thami</span>
-          </h1>
-          <h1 className="text-5xl z-10 font-medium leading-normal">
-            <span className="">Front-End</span> Developer
-          </h1>
-          <div className="text-xl my-5">
-            <p>
-              I've been working as a{" "}
-              <span className="font-bold">Front-End developer</span> for a
-              couple of years.
-            </p>
-            <p>
-              I am based in <span className="font-bold">Nepal</span>.
-            </p>
-          </div>
-          <button className="px-6 py-3 border border-[#DBF52F] bg-[#DBF52F] text-black font-medium rounded-sm hover:bg-black hover:text-white transition-all duration-150">
-            Hire Me
+        <div className="flex flex-col">
+          <p className="py-5 text-lg">
+            I've been working as a Front-End developer for 2 years. I'm based on
+            Nepal
+          </p>
+          <button className="bg-yellow border-yellow hover:border-yellow mx-auto rounded-sm border px-12 py-4 text-center font-semibold text-black transition-all duration-300 hover:bg-black hover:text-white">
+            Hire me
           </button>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
